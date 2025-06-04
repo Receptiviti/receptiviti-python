1cfefab78a495e42c9fa48b3d43d57168f806f03
Micah <micah.iserman@gmail.com>
2025-06-04 09:15:38 -0400

    fixes norming tests

--- tests/test_norming.py
+++ tests/test_norming.py
@@ -1,5 +1,6 @@
 import os
 
+import pandas
 import pytest
 
 import receptiviti
@@ -11,27 +12,27 @@ receptiviti.readin_env()
 class TestStatus:
     def test_listing(self):
         res = receptiviti.norming()
-        assert isinstance(res, dict) and isinstance(res["name"], dict)
+        assert isinstance(res, pandas.DataFrame)
         assert "custom/test" in res["name"].values
 
         res = receptiviti.norming(name_only=True)
-        assert isinstance(res, dict)
+        assert isinstance(res, list)
         assert "custom/test" in res
 
     def test_single_status(self):
         res = receptiviti.norming("test")
-        assert isinstance(res, dict) and isinstance(res["name"], dict)
+        assert isinstance(res, pandas.Series)
         assert "custom/test" == res["name"]
 
     def test_updating(self):
-        norming_context = "short_text"
+        norming_context = "test"
         receptiviti.norming(norming_context, delete=True)
         receptiviti.norming(norming_context, options={"min_word_count": 1})
         with pytest.raises(RuntimeError, match="is not on record"):
             receptiviti.request("a text to score", version="v2", custom_context=norming_context)
         receptiviti.norming(norming_context, "new text to add")
         final_status = receptiviti.norming(norming_context)
-        assert isinstance(final_status, dict)
+        assert isinstance(final_status, pandas.Series)
         assert final_status["status"] == "completed"
         base_request = receptiviti.request("a new text to add", version="v2")
         self_normed_request = receptiviti.request("a new text to add", version="v2", custom_context=norming_context)
